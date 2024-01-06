bf26b21c6e9b733e175fb9876e1be2b4621fa566
Azakit <155274165+AzakitHU@users.noreply.github.com>
2024-01-06 21:05:14 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,7 @@
 Locales["en"] = {
-    start_npc = "Sell",
+    ---SELL 1---
+    ------------
+    start_npc = "Sell Weed",
     check = "Let me see if I have the items needed for the heist.",
     noitem = "The required item is not available.",
     noitem2 = "You don't have enough!",
@@ -7,4 +9,18 @@ Locales["en"] = {
     Transportdraw = "[~g~E~w~] Knock",
     Sell = "Transfer in progress...",
     Deliveryblipname = "Delivery Point",
+    ---SELL 2---
+    ------------
+    start_npc2 = "Sell Coke",
+    check2 = "Let me see if I have the items needed for the heist.",
+    noitem2 = "The required item is not available.",
+    noitem2_2 = "You don't have enough!",
+    gotodelivery2 = "Go to the place marked on the map!",
+    ---SELL 3---
+    ------------
+    start_npc3 = "Sell Medikit",
+    check3 = "Let me see if I have the items needed for the heist.",
+    noitem3 = "The required item is not available.",
+    noitem2_3 = "You don't have enough!",
+    gotodelivery3 = "Go to the place marked on the map!",
 }
