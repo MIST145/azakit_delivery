0ee33144937435e78a075097985a5b7ef0fa7383
Azakit <155274165+AzakitHU@users.noreply.github.com>
2024-01-07 16:54:17 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -2,7 +2,7 @@ Locales["en"] = {
     ---SELL 1---
     ------------
     start_npc = "Sell Weed",
-    check = "Let me see if I have the items needed for the heist.",
+    check = "Let me see if I have the items needed.",
     noitem = "The required item is not available.",
     noitem2 = "You don't have enough!",
     gotodelivery = "Go to the place marked on the map!",    
@@ -12,14 +12,14 @@ Locales["en"] = {
     ---SELL 2---
     ------------
     start_npc2 = "Sell Coke",
-    check2 = "Let me see if I have the items needed for the heist.",
+    check2 = "Let me see if I have the items needed.",
     noitem2 = "The required item is not available.",
     noitem2_2 = "You don't have enough!",
     gotodelivery2 = "Go to the place marked on the map!",
     ---SELL 3---
     ------------
     start_npc3 = "Sell Medikit",
-    check3 = "Let me see if I have the items needed for the heist.",
+    check3 = "Let me see if I have the items needed.",
     noitem3 = "The required item is not available.",
     noitem2_3 = "You don't have enough!",
     gotodelivery3 = "Go to the place marked on the map!",
